7c43acf2224794237f00d36bdd35782ca74bc431
lxz07 <liuxiongzhuo@163.com>
2025-11-01 20:21:32 +0800

    have a good start

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,32 @@
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const uuid = '04e5d30d-7ccb-49b3-ad5f-4b07d45d8dfd'.replaceAll('-', '')
+		const path = '/04e5d30d'
+		if (new URL(request.url).pathname != path) {
+			return new Response('Hello World!');
+		}
+		const [client, server] = Object.values(new WebSocketPair())
+		if (request.headers.get('Upgrade') != 'websocket') {
+			return new Response(null)
+		}
+		server.accept()
+		server.addEventListener('message', (d) => {
+			const a = new Uint8Array(d.data)
+			const b = a.subarray(1, 17)
+			if (uuid != Array.from(b).map(c => c.toString(16).padStart(2, '0')).join('')) {
+				server.close()
+				return
+			}
+			const m = a[17]
+			console.log(m);
+			const cmd = a[18]
+			const portArray = a.subarray(19 + m, 19 + m + 2)
+			const port = portArray[0] * 256 + portArray[1]
+			console.log(port)
+			const addrType = a[20 + m]
+			server.close()
+		})
+		return new Response(null, { status: 101, webSocket: client })
 	},
 } satisfies ExportedHandler<Env>;
